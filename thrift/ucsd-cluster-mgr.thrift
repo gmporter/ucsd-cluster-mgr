b9cd95337b1aca4c6315b8fae19f8594a2600885
George Porter <gmporter@cs.ucsd.edu>
2012-12-11 11:05:01 -0800

    First take at a Thrift definition of the service interface

---
+++ thrift/ucsd-cluster-mgr.thrift
@@ -0,0 +1,123 @@
+
+
+#
+# Data structures
+#
+
+struct AuthenticationRequest {
+	# currently supported map keys: "username"
+	1: required map<string,string> credentials
+}
+
+enum HostStatus {
+	UNKNOWN = 0,
+	ASSIGNED = 1,
+	AVAILABLE = 2
+}
+
+struct Host {
+	1: required string name,
+	2: required HostStatus status,
+	3: required string owner,
+	4: required string assigned_project,
+	5: required bool netboot_enabled,
+	6: required string macaddr
+}
+
+struct Project {
+	1: required string name,
+	2: required string nfsrootpath,
+	3: required string kernel,
+	4: required string initrd,
+	5: list<string> parameters
+}
+
+struct User {
+	1: required string name,
+	2: string fullname
+}
+
+#
+# Exceptions
+#
+
+exception AuthenticationException {
+	1: required string why
+}
+
+exception ClientError {
+	1: string why
+}
+
+exception BadProjectException {
+}
+
+exception BadHostException {
+}
+
+exception BadUserException {
+}
+
+service ClusterManager {
+	#
+	# log-in / authentication
+	#
+	void login(1: required AuthenticationRequest auth_request)
+		throws (1: AuthenticationException authnx),
+	
+	#
+	# Management and meta routines
+	#
+
+	void ping(),
+
+	void host_add(1:required Host host),
+	void host_remove(1:required string host),
+
+	void project_add(1:required Project project),
+	void project_remove(1:required string project),
+
+	void user_add(1:required User user),
+	void user_remove(1:required string user),
+	
+	#
+	# project/host/tag retrieval methods
+	#
+	# we should fail if a bad project or host is given, but don't fail
+	# if a bad tag is given since we shouldn't have to pre-define tags
+	#
+
+	list<Project> get_projects(),
+
+	# project and/or tag can be specified to restrict the hosts returned
+	#  if you specify a tag, but no project, you get a client exception
+	#
+	list<Host> get_hosts(1:string project, 2:string tag)
+		throws (1:ClientError clix, 2:BadProjectException prjx),
+
+	list<string> get_tags(1:required string host)
+		throws (1:BadHostException hostx),
+
+	#
+	# project/host/tag modification methods
+	#
+
+	void host_assign(1:required string host,
+		 	 2:required string project,
+			 3:string user)
+		throws (1:BadHostException hostx,
+			2:BadProjectException projx,
+			3:BadUserException userx),
+
+	void host_release(1:required string host)
+		throws (1:BadHostException hostx),
+
+	void tag_add(1:required string host, 2:required string tag)
+		throws (1:BadHostException hostx),
+
+	void tag_remove(1:required string host, 2:required string tag)
+		throws (1:BadHostException hostx),
+
+	void tag_removeAll(1:required string host)
+		throws (1:BadHostException hostx),
+}
